d8757db0513f860d4b03e5c0d013d0fb173f1912
Martino Roma <mroma@genesicom.com>
2021-03-17 12:16:59 +0100

    ok

--- adtrapp.js
+++ adtrapp.js
@@ -1,24 +1,34 @@
 //const html = ``;
 // document.querySelector("#app").insertAdjacentHTML("afterbegin", html);
 
+const cliente = {
+  nome: "Agugiaro & Figna Molini S.p.A."
+};
+
 const app = {
   data() {
     return {
-      islamicExpertNome: "",
-      islamicExpertFunzione: "",
-      islamicExpertEnte: "Halal Italia Srl",
-      technicalExpertNome: "",
-      technicalExpertFunzione: "",
-      technicalExpertEnte: "Halal Italia Srl",
-      technicalAuditorNome: "",
-      technicalAuditorFunzione: "",
-      technicalAuditorEnte: "Halal Italia Srl"
+      result: {
+        islamicExpertNome: "",
+        islamicExpertFunzione: "",
+        islamicExpertEnte: "Halal Italia Srl",
+        technicalExpertNome: "",
+        technicalExpertFunzione: "",
+        technicalExpertEnte: "Halal Italia Srl",
+        technicalAuditorNome: "",
+        technicalAuditorFunzione: "",
+        technicalAuditorEnte: "Halal Italia Srl",
+        clienteNome: ""
+      }
     };
+  },
+  mounted() {
+    this.result.clienteNome = cliente.nome;
   }
 };
 
 const vm = Vue.createApp(app).mount("#app");
 
 const getData = function () {
-  return { ...vm.$data };
+  return { ...vm.$data.result };
 };
